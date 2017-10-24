74857fc71062c96f7740f645b9d00b0668bf6311
Alberto Tejos S <alb.tejos@gmail.com>
2017-10-24 16:56:56 -0300

    Main test

--- src/main/java/cl/duoc/misofertas/mail/MailSender.java
+++ src/main/java/cl/duoc/misofertas/mail/MailSender.java
@@ -1,6 +1,8 @@
 package cl.duoc.misofertas.mail;
 
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -9,6 +11,12 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.impl.StdSchedulerFactory;
+import static org.quartz.JobBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
 
 public class MailSender {
 
@@ -16,7 +24,7 @@ public class MailSender {
     private static final String PASSWORD = "misofertaspass";  //hackerman ¯\_(^-^)_/¯
     private static final Properties PROPS = new Properties();
 
-    private static  void loadProperties() {
+    private static void loadProperties() {
         PROPS.put("mail.smtp.starttls.enable", "true");
         PROPS.put("mail.smtp.auth", "true");
         PROPS.put("mail.smtp.host", "smtp.gmail.com");
@@ -25,7 +33,7 @@ public class MailSender {
 
     public static void send(String mailTo, String subject, String text) {
         loadProperties();
-        
+
         try {
             Session session = Session.getInstance(PROPS,
                     new javax.mail.Authenticator() {
@@ -45,6 +53,22 @@ public class MailSender {
 
         } catch (MessagingException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scheduler scheduler;
+        try {
+            scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+
+            while (true) {
+                MailSender.send("alb.tejos@gmail.com", "Asunto", "Probando Scheduler");
+            }
+
+            //scheduler.shutdown();
+        } catch (SchedulerException ex) {
+            Logger.getLogger(MailSender.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
